e6c2c31e2d4d6d1048669c6fa21d470373b39492
Suneel <suneel.kathait8189@gmail.com>
2017-05-14 14:26:44 +0530

    updated

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.get('/login', function(req, res, next) {
+ res.render('login', { title: 'Login' });
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
